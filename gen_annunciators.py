93542d34f8d2272ed2f5fe8ad73785c24abe3e0d
Simon Hollingshead <me@simonhollingshead.com>
2024-05-01 02:31:08 +0100

    Upload Annunciator Generator

---
+++ gen_annunciators.py
@@ -0,0 +1,30 @@
+import minify_html
+from string import Template
+from os import path
+
+class NoDollarTemplate(Template):
+    delimiter = '_tpl_'
+
+commons = {"filename": "commons.htm",
+           "house_lower": "commons",
+           "house_precap": "Commons",
+           "house_allcap": "COMMONS",
+           "house_id": 0,
+           "house_hex": "#005434",
+           "scroll_hex": "#9D0830"}
+
+lords = {"filename": "lords.htm",
+         "house_lower": "lords",
+         "house_precap": "Lords",
+         "house_allcap": "LORDS",
+         "house_id": 1,
+         "house_hex": "#9D0830",
+         "scroll_hex": "#005434"}
+
+with open(path.join(path.dirname(__file__), "template.htm"), "r") as f:
+    body = NoDollarTemplate(f.read())
+
+for params in [commons, lords]:
+    with open(path.join(path.dirname(__file__), params["filename"]), "w") as f:
+        f.write(body.substitute(params))
+        # f.write(minify_html.minify(body.substitute(params), minify_css=True, minify_js=True))
